24397f9323d3711cb5440333cbb5a5108e8d32a8
Mariana Villar <58176338+marianavillarp@users.noreply.github.com>
2021-04-20 17:38:09 +0100

    Delete User_Interface.java

--- User_Interface.java
+++
@@ -1,7 +0,0 @@
-package test_client;
-
-public interface User_Interface {
-    
-	// send messsage and contactServer() should be interface-defined and implemented diffferntly in client and admin classes
-
-}
